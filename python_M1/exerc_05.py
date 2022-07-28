a537b0bdf179b17cc8a0f04084a6d89c14da0ef1
mateus <mateusgodinho0102@gmail.com>
2022-07-28 00:39:11 -0300

    add_exerc_05.py

---
+++ python_M1/exerc_05.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input("Por favor informe um número: "))
+print(f"\nO número informado foi o {n}, o seu sucessor é o número {n + 1} e seu antecessor é o número {n - 1} ")
